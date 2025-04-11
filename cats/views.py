e45efe9a6685a37a17372afd77339924dab8c424
Ekaterina Kolomicheva <keaspb7@yandex.ru>
2025-04-11 16:47:18 +0300

    validation of serializator

--- cats/views.py
+++ cats/views.py
@@ -7,13 +7,19 @@ from .serializers import CatSerializer
 
 
 @api_view(['GET', 'POST'])
+@api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
+        # Создаём объект сериализатора 
+        # и передаём в него данные из POST-запроса
         serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
+            # Если полученные данные валидны —
+            # сохраняем данные в базу через save().
             serializer.save()
+            # Возвращаем JSON со всеми данными нового объекта
+            # и статус-код 201
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+        # Если данные не прошли валидацию — 
+        # возвращаем информацию об ошибках и соответствующий статус-код:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
